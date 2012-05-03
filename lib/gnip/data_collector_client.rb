ad1647be560666691ebc58da703b624fa8d56c4f
Andy Ogzewalla <andy@socialvolt.com>
2012-05-03 08:58:12 -0500

    Update DataCollectorClient to use libcurl bindings for streaming from Gnip

--- lib/gnip/data_collector_client.rb
+++ lib/gnip/data_collector_client.rb
@@ -4,36 +4,36 @@ require 'em-http'
 module Gnip
   class DataCollectorClient < Client
 
-    # Streams and parses data from Gnip.  Once an entire activity stream object (JSON) has been parsed, the
-    # resulting hash will be passed as an argument to the given block.
-    def stream
+    # Streams chunked data from Gnip and yields XML entries to the provided block
+    def stream(&block)
       raise "No block provided for call to #{self.class.name}#stream" unless block_given?
 
+      buffer = ''
+      entry = ''
+
       loop do
-        # run the EventMachine reactor, this call will block until 
-        # EventMachine.stop is called
-        EventMachine.run do
-          http = EventMachine::HttpRequest.new(@url).get :head => {'authorization' => [@username, @password]}
-          buffer = ''
-          entry = ''
-          http.stream do |chunk|
+        Curl::Easy.http_get @url do |c|
+          c.http_auth_types = :basic
+          c.username = @username
+          c.password = @password
+          c.encoding = 'gzip'
+          c.on_body do |chunk|
+
             buffer += chunk
             while line = buffer.slice!(/.+\r?\n/)
               entry << line
               if line.match(/<\/entry>/)
-                yield SergioActivityStreamPost.new(entry).to_json_activity_stream_hash
+                yield entry
                 entry = ''
               end
             end
+
+            chunk.size
           end
-
-          # when the HTTP request is done, stop EventMachine
-          http.callback { EventMachine.stop }
         end
-        sleep(0.25)
+
+        sleep(0.25) # Need to implement an exponential backoff pattern *
       end
-
     end
-
   end
 end
